36df1dc012a732d113713340f52bd88d1f63c704
ayoub2001 <jebbouri1968@gmail.com>
2023-07-17 12:09:34 -0700

    eating software

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first_digit1, second_digit1, first_digit2, second_digit2;
+
+	for (int num1 = 0; num1 <= 99; num1++)
+	{
+		first_digit1 = num1 / 10;
+		second_digit1 = num1 % 10;
+		for (int num2 = num1; num2 <= 99; num2++)
+		{
+			first_digit2 = num2 / 10;
+			second_digit2 = num2 % 10;
+			if (num1 <= num2)
+			{
+				putchar(first_digit1 / 10 + '0');
+				putchar(first_digit1 % 10 + '0');
+				putchar(second_digit1 / 10 + '0');
+				putchar(second_digit1 % 10 + '0');
+				putchar(' ');
+				putchar(first_digit2 / 10 + '0');
+				putchar(first_digit2 % 10 + '0');
+				putchar(second_digit2 / 10 + '0');
+				putchar(second_digit2 % 10 + '0');
+				if (num1 != 99 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
