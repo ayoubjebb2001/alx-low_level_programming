353a4cc21ce6a5f33a22380198e1d7ea51aee37f
ayoub2001 <jebbouri1968@gmail.com>
2023-07-17 09:24:13 -0700

    fix loop

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter < 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		if (letter != 'q' && letter != 'e')
 		putchar (letter);
 	putchar('\n');
